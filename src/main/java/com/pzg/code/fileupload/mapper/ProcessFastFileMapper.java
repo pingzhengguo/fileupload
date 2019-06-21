de2c6eec485bd74e84fe7bd9d3c5030d56fb09e7
pingzhengguo <pingzhengguo@hiynn.com>
2019-06-21 17:54:34 +0800

    文件上传

---
+++ src/main/java/com/pzg/code/fileupload/mapper/ProcessFastFileMapper.java
@@ -0,0 +1,15 @@
+package com.pzg.code.fileupload.mapper;
+
+import com.pzg.code.fileupload.entity.ProcessFastFile;
+import com.pzg.code.fileupload.utils.MyMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author : PZG
+ * @date : 2018-09-04   10:48
+ * @description :
+ */
+@Mapper
+public interface ProcessFastFileMapper extends MyMapper<ProcessFastFile> {
+
+}
